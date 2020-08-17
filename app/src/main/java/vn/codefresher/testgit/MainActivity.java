a442168fa759817fe2118118cd30f815a081612e
DESKTOP-ROJUKPD\ADMIN_TD <vanthuan080896@gmail.com>
2020-08-17 19:52:08 +0700

    init

--- app/src/main/java/vn/codefresher/testgit/MainActivity.java
+++ app/src/main/java/vn/codefresher/testgit/MainActivity.java
@@ -3,12 +3,17 @@ package vn.codefresher.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv_hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init() {
+        tv_hello = findViewById(R.id.tv_hello);
     }
 }
