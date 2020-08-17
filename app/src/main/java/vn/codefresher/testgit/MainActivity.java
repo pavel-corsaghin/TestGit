ed2330a3e3e4b75e5b7133cb14778ad97a288099
DESKTOP-ROJUKPD\ADMIN_TD <vanthuan080896@gmail.com>
2020-08-17 20:04:17 +0700

    activity_main

--- app/src/main/java/vn/codefresher/testgit/MainActivity.java
+++ app/src/main/java/vn/codefresher/testgit/MainActivity.java
@@ -3,10 +3,15 @@ package vn.codefresher.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv_hello;
+    EditText edt_user, edt_pass;
+    Button btn_login;
+    CheckBox cb_remember;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
     private void init() {
-        tv_hello = findViewById(R.id.tv_hello);
+        edt_user = findViewById(R.id.username);
+        edt_pass = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+        cb_remember = findViewById(R.id.cb_remember);
     }
 }
